732fa6268fcaec33fa0a854a34b82b344461d512
kurtislin <klaus.lin.sc@gmail.com>
2025-03-09 23:18:39 +0800

    Implement q_new and q_free functions
    Created queue initialization and cleanup functions that handle
    memory allocation and deallocation properly.
    
    Change-Id: I082e4c82eadddfa50c00aeba0f59a3f41c81bef8

--- queue.c
+++ queue.c
@@ -13,11 +13,28 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    while (!list_empty(head)) {
+        struct list_head *node = head->next;
+        element_t *element = list_entry(node, element_t, list);
+        list_del(node);
+        q_release_element(element);
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
